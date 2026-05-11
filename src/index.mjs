0dc616f2d5c27242afa7e456d62cae9068ff0cba
ijvadeli <idhvandelindt@icloud.com>
2026-05-11 10:57:56 +0200

    Added mjs file

--- src/index.mjs
+++ src/index.mjs
@@ -2,9 +2,11 @@
 import express from "express";
 const app = express();
 const port = "3000";
+var path = require('path');
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+  res.send("./cat.png");
   console.log("Response sent");
 });
 
